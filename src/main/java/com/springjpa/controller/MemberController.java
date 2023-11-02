474b5a26c893b49166cdb4b5b12ccb63d9965bab
choidongkuen <danaver12@daum.net>
2023-11-02 18:39:49 +0900

    feat : Web 확장 - 도메인 클래스 컨버터 실습

--- src/main/java/com/springjpa/controller/MemberController.java
+++ src/main/java/com/springjpa/controller/MemberController.java
@@ -4,17 +4,14 @@ import com.springjpa.dto.MemberDto;
 import com.springjpa.entity.Member;
 import com.springjpa.repository.MemberRepository;
 import com.springjpa.service.MemberService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,6 +19,7 @@ import java.util.stream.Collectors;
 public class MemberController {
 
     private final MemberService memberService;
+    private final MemberRepository memberRepository;
 
 
     // Page, Slice 형식 으로 반환 하는 것 Good!
@@ -37,5 +35,14 @@ public class MemberController {
             @RequestParam int age
     ) {
         return memberService.getMemberDtoUSingPagingBySlice(age);
+    }
+
+    /**
+     * - 도메인 클래스 컨버터
+     * - only 조회 용도
+     **/
+    @GetMapping("/member/{id}")
+    public String findMember(@PathVariable("id") Member member) {
+        return member.getName();
     }
 }
